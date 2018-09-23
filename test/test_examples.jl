c9b2bfdf99e71162b8acef07dff517a93461f199
Samuele Carcagno <sam.carcagno@gmail.com>
2018-09-23 01:55:42 +0100

    track test_examples.jl in git

---
+++ test/test_examples.jl
@@ -0,0 +1,31 @@
+using BrainWaveExperimental
+
+fToProcess = ["BrainWaveExperimental.jl", "findABRPeaks.jl", "utils.jl"]
+for fName in fToProcess
+#fName = fToProcess[1]
+    fIn = open(string("../src/", fName), "r")
+    fOut = open(string("test_", fName), "w")
+    lns = readlines(fIn)
+    idxStart = (Int)[]
+    idxStop = (Int)[]
+    for i=1:length(lns)
+        if lns[i] == "```julia"
+            push!(idxStart, i+1)
+        elseif lns[i] == "```"
+            push!(idxStop, i-1)
+        end
+    end
+    for i=1:length(idxStart)
+        theseLines = lns[idxStart[i]:idxStop[i]]
+        for currLine in theseLines
+            write(fOut, string(currLine, "\n"))
+        end
+    end
+    close(fIn); close(fOut)
+end
+
+for fName in fToProcess
+    fNameTest = string("test_", fName)
+    include(fNameTest)
+end
+
